827611d3b3e8dfa863bd61212db01dfe30aabeb5
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-07 10:55:46 +0100

    Containing current monsters as a list

---
+++ Monsters.py
@@ -0,0 +1,42 @@
+monsters = [
+    {
+    "name": "Blue eyes",
+    "attack": 3000,
+},
+{
+    "name": "Dark magician",
+    "attack": 2400,
+},
+{
+    "name": "Stardust Dragon",
+    "attack": 2600,
+},
+{
+    "name": "Red Dragon Archfiend",
+    "attack": 3000,
+},
+{
+    "name": "Cyber dragon",
+    "attack": 2200,
+},
+{
+    "name": "Black rose dragon",
+    "attack": 2500,
+},
+{
+    "name": "Crystal wing synchro dragon",
+    "attack": 2700,
+},
+{
+    "name": "Dark magician girl",
+    "attack": 2300,
+},
+{
+    "name": "exodia",
+    "attack": 2800,
+},
+{
+    "name": "Junk warrior",
+    "attack": 2300,
+}
+]
